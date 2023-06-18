9e3b1cc6a96b585f64b07b3d1a73b15fd9442f13
Christopher Rabotin <christopher.rabotin@gmail.com>
2023-06-18 15:17:02 -0600

    ID based LUT works

--- src/structure/dataset.rs
+++ src/structure/dataset.rs
@@ -7,40 +7,73 @@
  *
  * Documentation: https://nyxspace.com/
  */
-use der::{asn1::SequenceOf, Decode, Encode, Reader, Writer};
+use crate::{errors::IntegrityErrorKind, prelude::AniseError};
+use der::{asn1::OctetStringRef, Decode, Encode, Reader, Writer};
 
-use super::{lookuptable::LookUpTable, metadata::Metadata, records::Record};
-
-pub const MAX_RECORDS: usize = 32;
+use super::{lookuptable::LookUpTable, metadata::Metadata};
 
 /// A DataSet is the core structure shared by all ANISE binary data.
 #[derive(Clone, Default)]
-pub struct DataSet<'a, R: Record<'a>> {
+pub struct DataSet<'a> {
     pub metadata: Metadata<'a>,
     /// All datasets have LookUpTable (LUT) that stores the mapping between a key and its index in the ephemeris list.
     pub lut: LookUpTable<'a>,
-    /// All datasets have up Records that store the actual data.
-    pub data: SequenceOf<R, MAX_RECORDS>,
+    pub crc32_checksum: u32,
+    /// The actual data from the dataset
+    pub bytes: &'a [u8],
 }
 
-impl<'a, R: Record<'a>> Encode for DataSet<'a, R> {
+impl<'a> DataSet<'a> {
+    /// Compute the CRC32 of the underlying bytes
+    pub fn crc32(&self) -> u32 {
+        crc32fast::hash(&self.bytes)
+    }
+
+    /// Scrubs the data by computing the CRC32 of the bytes and making sure that it still matches the previously known hash
+    pub fn scrub(&self) -> Result<(), AniseError> {
+        if self.crc32() == self.crc32_checksum {
+            Ok(())
+        } else {
+            // Compiler will optimize the double computation away
+            Err(AniseError::IntegrityError(
+                IntegrityErrorKind::ChecksumInvalid {
+                    expected: self.crc32_checksum,
+                    computed: self.crc32(),
+                },
+            ))
+        }
+    }
+}
+
+impl<'a> Encode for DataSet<'a> {
     fn encoded_len(&self) -> der::Result<der::Length> {
-        self.metadata.encoded_len()? + self.lut.encoded_len()? + self.data.encoded_len()?
+        let as_byte_ref = OctetStringRef::new(&self.bytes)?;
+        self.metadata.encoded_len()?
+            + self.lut.encoded_len()?
+            + self.crc32_checksum.encoded_len()?
+            + as_byte_ref.encoded_len()?
     }
 
     fn encode(&self, encoder: &mut dyn Writer) -> der::Result<()> {
+        let as_byte_ref = OctetStringRef::new(&self.bytes)?;
         self.metadata.encode(encoder)?;
         self.lut.encode(encoder)?;
-        self.data.encode(encoder)
+        self.crc32_checksum.encode(encoder)?;
+        as_byte_ref.encode(encoder)
     }
 }
 
-impl<'a, R: Record<'a>> Decode<'a> for DataSet<'a, R> {
+impl<'a> Decode<'a> for DataSet<'a> {
     fn decode<D: Reader<'a>>(decoder: &mut D) -> der::Result<Self> {
+        let metadata = decoder.decode()?;
+        let lut = decoder.decode()?;
+        let crc32_checksum = decoder.decode()?;
+        let bytes: OctetStringRef = decoder.decode()?;
         Ok(Self {
-            metadata: decoder.decode()?,
-            lut: decoder.decode()?,
-            data: decoder.decode()?,
+            metadata,
+            lut,
+            crc32_checksum,
+            bytes: bytes.as_bytes(),
         })
     }
 }
--- src/structure/lookuptable.rs
+++ src/structure/lookuptable.rs
@@ -7,83 +7,200 @@
  *
  * Documentation: https://nyxspace.com/
  */
-use der::{Decode, Encode, Reader, Writer};
-use log::error;
+use der::{
+    asn1::{OctetStringRef, SequenceOf},
+    Decode, Encode, Reader, Writer,
+};
+use heapless::FnvIndexMap;
+use log::warn;
 
 use crate::{prelude::AniseError, NaifId};
 
-use super::array::DataArray;
+pub const MAX_LUT_ENTRIES: usize = 32;
 
-/// A LookUpTable allows looking up the data given the hash.
+/// A lookup table entry contains the start and end indexes in the data array of the data that is sought after.
+///
+/// # Implementation note
+/// This data is stored as a u32 to ensure that the same binary representation works on all platforms.
+/// In fact, the size of the usize type varies based on whether this is a 32 or 64 bit platform.
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
+pub struct Entry {
+    pub start_idx: u32,
+    pub end_idx: u32,
+}
+
+impl Encode for Entry {
+    fn encoded_len(&self) -> der::Result<der::Length> {
+        self.start_idx.encoded_len()? + self.end_idx.encoded_len()?
+    }
+
+    fn encode(&self, encoder: &mut dyn Writer) -> der::Result<()> {
+        self.start_idx.encode(encoder)?;
+        self.end_idx.encode(encoder)
+    }
+}
+
+impl<'a> Decode<'a> for Entry {
+    fn decode<R: Reader<'a>>(decoder: &mut R) -> der::Result<Self> {
+        Ok(Self {
+            start_idx: decoder.decode()?,
+            end_idx: decoder.decode()?,
+        })
+    }
+}
+
+/// A LookUpTable allows finding the [Entry] associated with either an ID or a name.
 ///
 /// # Note
-/// In this version of ANISE, the look up is O(N) due to a limitation in the ASN1 library used.
-/// Eventually, the specification will require the hashes will be ordered for a binary search on the index,
-/// thereby greatly reducing the search time for each data, from O(N) to O(log N).
-#[derive(Clone, Default, PartialEq, Eq)]
+/// _Both_ the IDs and the name MUST be unique in the look up table.
+#[derive(Clone, Default, Debug, PartialEq, Eq)]
 pub struct LookUpTable<'a> {
     /// Unique IDs of each item in the
-    pub hashes: DataArray<'a, NaifId>,
+    pub by_id: FnvIndexMap<NaifId, Entry, MAX_LUT_ENTRIES>,
     /// Corresponding index for each hash
-    pub indexes: DataArray<'a, u16>,
+    pub by_name: FnvIndexMap<&'a str, Entry, MAX_LUT_ENTRIES>,
 }
 
 impl<'a> LookUpTable<'a> {
-    pub const fn len(&self) -> usize {
-        self.indexes.len()
-    }
-    /// Searches the lookup table for the requested hash
-    /// Returns Ok with the index for the requested hash
-    /// Returns Err with an ItemNotFound if the item isn't found
-    /// Returns Err with an IndexingError if the index is not present but the hash is present.
-    ///
-    /// NOTE: Until https://github.com/anise-toolkit/anise.rs/issues/18 is addressed
-    /// this function has a time complexity of O(N)
-    pub fn index_for_hash(&self, hash: &NaifId) -> Result<u16, AniseError> {
-        for (idx, item) in self.hashes.data.iter().enumerate() {
-            if item == hash {
-                return match self.indexes.data.get(idx) {
-                    Some(item_index) => Ok(*item_index),
-                    None => {
-                        error!("lookup table contain {hash:x} ({hash}) but it does not have an entry for it");
-                        // TODO: Change to integrity error
-                        Err(AniseError::MalformedData(idx))
-                    }
-                };
-            }
-        }
-        Err(AniseError::ItemNotFound)
+    pub fn append(&mut self, id: i32, name: &'a str, entry: Entry) -> Result<(), AniseError> {
+        self.by_id
+            .insert(id, entry)
+            .map_err(|_| AniseError::StructureIsFull)?;
+        self.by_name
+            .insert(name, entry)
+            .map_err(|_| AniseError::StructureIsFull)?;
+        Ok(())
     }
 
-    /// Searches the lookup table for the requested key
-    /// Returns Ok with the index for the hash of the requested key
-    /// Returns Err with an ItemNotFound if the item isn't found
-    /// Returns Err with an IndexingError if the index is not present but the hash of the name is present.
-    ///
-    /// NOTE: Until https://github.com/anise-toolkit/anise.rs/issues/18 is addressed
-    /// this function has a time complexity of O(N)
-    pub fn index_for_key(&self, _key: &str) -> Result<u16, AniseError> {
-        // self.index_for_hash(&hash(key.as_bytes()))
-        todo!()
+    pub fn append_id(&mut self, id: i32, entry: Entry) -> Result<(), AniseError> {
+        self.by_id
+            .insert(id, entry)
+            .map_err(|_| AniseError::StructureIsFull)?;
+        Ok(())
+    }
+
+    pub fn append_name(&mut self, name: &'a str, entry: Entry) -> Result<(), AniseError> {
+        self.by_name
+            .insert(name, entry)
+            .map_err(|_| AniseError::StructureIsFull)?;
+        Ok(())
+    }
+
+    /// Builds the DER encoding of this look up table
+    fn der_encoding(
+        &self,
+    ) -> (
+        SequenceOf<i32, MAX_LUT_ENTRIES>,
+        SequenceOf<OctetStringRef, MAX_LUT_ENTRIES>,
+        SequenceOf<Entry, MAX_LUT_ENTRIES>,
+    ) {
+        // Decide whether to encode the entries from the ID iterator or the names iterator based on which has the most.
+        let use_id = self.by_id.len() >= self.by_name.len();
+        // Build the list of entries
+        let mut entries = SequenceOf::<Entry, MAX_LUT_ENTRIES>::new();
+        // Build the list of keys
+        let mut ids = SequenceOf::<i32, MAX_LUT_ENTRIES>::new();
+        for (id, entry) in &self.by_id {
+            ids.add(*id).unwrap();
+            if use_id {
+                entries.add(*entry).unwrap();
+            }
+        }
+        // Build the list of names
+        let mut names = SequenceOf::<OctetStringRef, MAX_LUT_ENTRIES>::new();
+        for (name, entry) in &self.by_name {
+            names
+                .add(OctetStringRef::new(name.as_bytes()).unwrap())
+                .unwrap();
+            if !use_id {
+                entries.add(*entry).unwrap();
+            }
+        }
+
+        (ids, names, entries)
     }
 }
 
 impl<'a> Encode for LookUpTable<'a> {
     fn encoded_len(&self) -> der::Result<der::Length> {
-        self.hashes.encoded_len()? + self.indexes.encoded_len()?
+        let (ids, names, entries) = self.der_encoding();
+        ids.encoded_len()? + names.encoded_len()? + entries.encoded_len()?
     }
 
     fn encode(&self, encoder: &mut dyn Writer) -> der::Result<()> {
-        self.hashes.encode(encoder)?;
-        self.indexes.encode(encoder)
+        let (ids, names, entries) = self.der_encoding();
+        ids.encode(encoder)?;
+        names.encode(encoder)?;
+        entries.encode(encoder)
     }
 }
 
 impl<'a> Decode<'a> for LookUpTable<'a> {
     fn decode<R: Reader<'a>>(decoder: &mut R) -> der::Result<Self> {
-        Ok(Self {
-            hashes: decoder.decode()?,
-            indexes: decoder.decode()?,
-        })
+        // Decode as sequences and use that to build the look up table.
+        let mut lut = Self::default();
+        let ids: SequenceOf<i32, MAX_LUT_ENTRIES> = decoder.decode()?;
+        let names: SequenceOf<OctetStringRef, MAX_LUT_ENTRIES> = decoder.decode()?;
+        let entries: SequenceOf<Entry, MAX_LUT_ENTRIES> = decoder.decode()?;
+        for (id, entry) in ids.iter().zip(entries.iter()) {
+            lut.by_id.insert(*id, *entry).unwrap();
+        }
+        for (name, entry) in names.iter().zip(entries.iter()) {
+            lut.by_name
+                .insert(core::str::from_utf8(name.as_bytes()).unwrap(), *entry)
+                .unwrap();
+        }
+        if lut.by_name.len() != lut.by_id.len() && !lut.by_id.is_empty() && !lut.by_name.is_empty()
+        {
+            warn!(
+                "decoded lookup table inconsistent: {} names but {} ids",
+                lut.by_name.len(),
+                lut.by_id.len()
+            );
+        }
+        Ok(lut)
+    }
+}
+
+#[cfg(test)]
+mod lut_ut {
+    use super::{Decode, Encode, Entry, LookUpTable, MAX_LUT_ENTRIES};
+    #[test]
+    fn zero_repr() {
+        let repr = LookUpTable::default();
+
+        let mut buf = vec![];
+        repr.encode_to_vec(&mut buf).unwrap();
+
+        let repr_dec = LookUpTable::from_der(&buf).unwrap();
+
+        assert_eq!(repr, repr_dec);
+
+        dbg!(repr);
+        dbg!(core::mem::size_of::<LookUpTable>());
+    }
+
+    #[test]
+    fn repr_ids_only() {
+        let mut repr = LookUpTable::default();
+        let num_bytes = 363;
+        for i in 0..(MAX_LUT_ENTRIES as u32) {
+            let id = -20 - (i as i32);
+            repr.append_id(
+                id,
+                Entry {
+                    start_idx: (i * num_bytes) as u32,
+                    end_idx: ((i + 1) * num_bytes) as u32,
+                },
+            )
+            .unwrap();
+        }
+
+        let mut buf = vec![];
+        repr.encode_to_vec(&mut buf).unwrap();
+
+        let repr_dec = LookUpTable::from_der(&buf).unwrap();
+
+        assert_eq!(repr, repr_dec);
     }
 }
