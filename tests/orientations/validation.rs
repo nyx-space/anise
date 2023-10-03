bbb1e82351954a68438f5471ad334a67193e3b9a
Christopher Rabotin <christopher.rabotin@gmail.com>
2023-10-02 23:36:26 -0600

    Validate IAU Jupiter rotation to parent
    This one has nutation and precession coefficients

--- tests/orientations/validation.rs
+++ tests/orientations/validation.rs
@@ -95,3 +95,76 @@ fn pck00008_iau_earth_validation() {
         );
     }
 }
+
+/// IAU Jupiter contains nutation and precession coefficients.
+#[test]
+fn pck00008_iau_jupiter_validation() {
+    let pck = "data/pck00008.tpc";
+    spice::furnsh(pck);
+    let planetary_data = convert_tpc(pck, "data/gm_de431.tpc").unwrap();
+
+    let mut almanac = Almanac::default();
+    almanac.planetary_data = planetary_data;
+
+    let iau_earth = Frame::from_ephem_orient(599, 599);
+
+    for (num, epoch) in TimeSeries::inclusive(
+        Epoch::from_tdb_duration(Duration::ZERO),
+        Epoch::from_tdb_duration(0.2.centuries()),
+        1.days(),
+    )
+    .enumerate()
+    {
+        let rot_data = spice::pxform("J2000", "IAU_JUPITER", epoch.to_tdb_seconds());
+        // Confirmed that the M3x3 below is the correct representation from SPICE by using the mxv spice function and compare that to the nalgebra equivalent computation.
+        let spice_mat = Matrix3::new(
+            rot_data[0][0],
+            rot_data[0][1],
+            rot_data[0][2],
+            rot_data[1][0],
+            rot_data[1][1],
+            rot_data[1][2],
+            rot_data[2][0],
+            rot_data[2][1],
+            rot_data[2][2],
+        );
+
+        let dcm = almanac.rotation_to_parent(iau_earth, epoch).unwrap();
+
+        let spice_dcm = DCM {
+            rot_mat: spice_mat,
+            from: dcm.from,
+            to: dcm.to,
+            rot_mat_dt: None,
+        };
+
+        // Compute the different in PRV and rotation angle
+        let q_anise = Quaternion::from(dcm);
+        let q_spice = Quaternion::from(spice_dcm);
+
+        let (anise_uvec, anise_angle) = q_anise.uvec_angle();
+        let (spice_uvec, spice_angle) = q_spice.uvec_angle();
+
+        let uvec_angle_deg_err = anise_uvec.dot(&spice_uvec).acos().to_degrees();
+        let deg_err = (anise_angle - spice_angle).to_degrees();
+
+        // In some cases, the arc cos of the angle between the unit vectors is NaN (because the dot product is rounded just past -1 or +1)
+        // so we allow NaN.
+        // However, we also check the rotation about that unit vector AND we check that the DCMs match too.
+        assert!(
+            uvec_angle_deg_err.abs() < MAX_ERR_DEG || uvec_angle_deg_err.is_nan(),
+            "#{num} @ {epoch} unit vector angle error"
+        );
+        assert!(
+            deg_err.abs() < MAX_ERR_DEG,
+            "#{num} @ {epoch} rotation error"
+        );
+
+        assert!(
+            (dcm.rot_mat - spice_mat).norm() < DCM_EPSILON,
+            "#{num} {epoch}\ngot: {}want:{spice_mat}err: {:.3e}",
+            dcm.rot_mat,
+            (dcm.rot_mat - spice_mat).norm()
+        );
+    }
+}
