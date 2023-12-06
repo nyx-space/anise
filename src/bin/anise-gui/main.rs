7c2c03acdd85c0f93644f6a69bfb8d02a9e184bf
Thomas Antony <mail@thomasantony.com>
2023-12-05 18:49:57 -0800

    Add entrypoint for web

--- src/bin/anise-gui/main.rs
+++ src/bin/anise-gui/main.rs
@@ -1,13 +1,14 @@
-use pretty_env_logger;
-use std::env::{set_var, var};
-
+#[allow(dead_code)]
 const LOG_VAR: &str = "ANISE_LOG";
 
 mod ui;
 
 use ui::UiApp;
 
+#[cfg(not(target_arch = "wasm32"))]
 fn main() {
+    use std::env::{set_var, var};
+
     if var(LOG_VAR).is_err() {
         set_var(LOG_VAR, "INFO");
     }
@@ -19,4 +20,25 @@ fn main() {
         eframe::NativeOptions::default(),
         Box::new(|cc| Box::new(UiApp::new(cc))),
     );
+}
+
+// Entrypoint for WebAssembly
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    use log::info;
+
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+    let web_options = eframe::WebOptions::default();
+
+    info!("Starting ANISE in WebAssembly mode");
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::WebRunner::new()
+            .start(
+                "anise_canvas",
+                web_options,
+                Box::new(|cc| Box::new(UiApp::new(cc))),
+            )
+            .await
+            .expect("failed to start eframe");
+    });
 }
