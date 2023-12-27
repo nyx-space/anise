3b70a17da5676ee70ffa6f5a8401732160feced8
Christopher Rabotin <christopher.rabotin@gmail.com>
2023-12-26 17:28:03 -0700

    [python] Add hifitime

--- anise-py/src/lib.rs
+++ anise-py/src/lib.rs
@@ -1,14 +1,41 @@
-use pyo3::prelude::*;
+/*
+ * ANISE Toolkit
+ * Copyright (C) 2021-2023 Christopher Rabotin <christopher.rabotin@gmail.com> et al. (cf. AUTHORS.md)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Documentation: https://nyxspace.com/
+ */
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
+use hifitime::leap_seconds::{LatestLeapSeconds, LeapSecondsFile};
+use hifitime::prelude::*;
+use hifitime::ut1::Ut1Provider;
+
+use pyo3::prelude::*;
+use pyo3::py_run;
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn anise(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+fn anise(py: Python, m: &PyModule) -> PyResult<()> {
+    register_time_module(py, m)?;
+    Ok(())
+}
+
+/// Reexport hifitime as nyx_space.time
+fn register_time_module(py: Python<'_>, parent_module: &PyModule) -> PyResult<()> {
+    let sm = PyModule::new(py, "_anise.time")?;
+
+    sm.add_class::<Epoch>()?;
+    sm.add_class::<TimeScale>()?;
+    sm.add_class::<TimeSeries>()?;
+    sm.add_class::<Duration>()?;
+    sm.add_class::<Unit>()?;
+    sm.add_class::<LatestLeapSeconds>()?;
+    sm.add_class::<LeapSecondsFile>()?;
+    sm.add_class::<Ut1Provider>()?;
+
+    py_run!(py, sm, "import sys; sys.modules['anise.time'] = sm");
+    parent_module.add_submodule(sm)?;
     Ok(())
 }
