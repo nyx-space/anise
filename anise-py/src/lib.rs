87ad34dc218402c2608061eed3f9f8153688e420
Christopher Rabotin <christopher.rabotin@gmail.com>
2024-01-09 18:11:43 -0700

    Export convert_tpc and convert_fk to Python

--- anise-py/src/lib.rs
+++ anise-py/src/lib.rs
@@ -19,12 +19,14 @@ use pyo3::prelude::*;
 use pyo3::py_run;
 
 mod astro;
+mod utils;
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn anise(py: Python, m: &PyModule) -> PyResult<()> {
     register_time_module(py, m)?;
     astro::register_astro(py, m)?;
+    utils::register_utils(py, m)?;
     m.add_class::<Almanac>()?;
     m.add_class::<Aberration>()?;
     m.add_class::<MetaAlmanac>()?;
