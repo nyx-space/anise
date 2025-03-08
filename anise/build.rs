9393582b325d25bb5f4d73fdede0bdf945a56586
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2025-03-08 08:32:31 +0100

    lints
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- anise/build.rs
+++ anise/build.rs
@@ -30,27 +30,26 @@ fn main() {
     let data_path = Path::new(&env!("CARGO_MANIFEST_DIR")).join("../data");
 
     // Create the directory if it doesn't exist
-    if !data_path.exists() {
-        if fs::create_dir_all(&data_path).is_err() {
-            eprintln!("EMBEDDED EPHEM UNAVAILABLE: failed to create directory {data_path:?}");
-            // Try nothing else.
-            return;
-        }
+    if !data_path.exists() && fs::create_dir_all(&data_path).is_err() {
+        eprintln!("EMBEDDED EPHEM UNAVAILABLE: failed to create directory {data_path:?}");
+        // Try nothing else.
+        return;
     }
 
     for (url, dest_path) in embedded_files {
         let resp = client
             .get(url)
             .send()
-            .expect(&format!("could not download {url}"));
+            .unwrap_or_else(|e| panic!("could not download {url}: {}", e));
 
         let bytes = resp
             .bytes()
-            .expect(&format!("could not read bytes from {url}"));
+            .unwrap_or_else(|e| panic!("could not read bytes from {url}: {}", e));
 
-        let mut file =
-            File::create(&dest_path).expect(&format!("could not create the data path {dest_path}"));
+        let mut file = File::create(&dest_path)
+            .unwrap_or_else(|e| panic!("could not create the data path {dest_path}: {}", e));
+
         file.write_all(&bytes)
-            .expect(&format!("could not write asset data to {dest_path}"));
+            .unwrap_or_else(|e| panic!("could not write asset to {dest_path}: {}", e));
     }
 }
