6e92f687ebbbf985ee35ca147bccf867cff0bcea
Christopher Rabotin <christopher.rabotin@gmail.com>
2025-09-04 22:54:07 -0600

    Proposed design for calculations is inconvenient
    StateDef is not copiable anymore, so passing it to all expressions requires a clone
    
    Booting serialization to subsequent issue

--- anise/src/analysis/mod.rs
+++ anise/src/analysis/mod.rs
@@ -11,33 +11,38 @@
 // FOCI: 1. Build the angle between two objects, defined in the loaded Almanac.
 //       2. Rebuild the angular momentum vector to demonstrate the cross product.
 
-use crate::prelude::FrameUid;
-use serde_derive::{Deserialize, Serialize};
-use serde_dhall::StaticType;
+use crate::{almanac::Almanac, prelude::FrameUid};
+// TODO: Once https://github.com/Nadrieril/dhall-rust/issues/242 is closed, enable Dhall serialization.
+// Will be implemented in https://github.com/nyx-space/anise/issues/466
+// use serde_derive::{Deserialize, Serialize};
+// use serde_dhall::StaticType;
 
-#[derive(Copy, Clone, Debug, PartialEq, Serialize, Deserialize, StaticType)]
-pub struct State {
-    from_frame: FrameUid,
-    to_frame: FrameUid,
+#[derive(Clone, Debug, PartialEq)]
+pub enum FrameDef {
+    Uid(FrameUid),
+    Manual(Box<ReferenceFrame>),
+}
+
+/// StateDef allows defining a state from one frame (`from_frame`) to another (`to_frame`)
+#[derive(Clone, Debug, PartialEq)]
+pub struct StateDef {
+    from_frame: FrameDef,
+    to_frame: FrameDef,
 }
 
 /// VectorExpr defines a vector expression, which can either be computed from a state, or from a fixed definition.
-/// It will eventually support building new reference frames at runtime using a CrossProduct operation.
-#[derive(Copy, Clone, Serialize, Deserialize, Debug, PartialEq, StaticType)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum VectorExpr {
     Fixed { x: f64, y: f64, z: f64 }, // Unitless vector, for arbitrary computations
-    Position(State),
-    Velocity(State),
-    OrbitalMomentum(State),
-    EccentricityVector(State), /* TODO: Once https://github.com/Nadrieril/dhall-rust/issues/242 is closed
-                               CrossProduct {
-                                  a: Box<Self>,
-                                  b: Box<Self>,
-                               },*/
+    Position(StateDef),
+    Velocity(StateDef),
+    OrbitalMomentum(StateDef),
+    EccentricityVector(StateDef),
+    CrossProduct { a: Box<Self>, b: Box<Self> },
 }
 
 /// VectorScalar defines a scalar computation from a (set of) vector expression(s).
-#[derive(Copy, Clone, Debug, PartialEq, Serialize, Deserialize, StaticType)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum ScalarCalc {
     Norm(VectorExpr),
     NormSquared(VectorExpr),
@@ -47,16 +52,15 @@ pub enum ScalarCalc {
 }
 
 /// Orbital element defines all of the supported orbital elements in ANISE, which are all built from a State.
-#[derive(Copy, Clone, Debug, PartialEq, Serialize, Deserialize, StaticType)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum OrbitalElement {
-    SemiMajorAxis(State),
-    RAAN(State),
-    Eccentricity(State),
+    SemiMajorAxis(StateDef),
+    RAAN(StateDef),
+    Eccentricity(StateDef),
 }
 
-/*  OrthogonalFrame also requires recursive types.
 // Defines how to build an orthogonal frame from custom vector expressions
-#[derive(Copy, Clone, Debug, PartialEq, Serialize, Deserialize, StaticType)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum OrthogonalFrame {
     CrossProductXY { x: VectorExpr, y: VectorExpr },
     CrossProductXZ { x: VectorExpr, z: VectorExpr },
@@ -65,25 +69,32 @@ pub enum OrthogonalFrame {
 
 /// Defines a runtime reference frame from an orthogonal frame, allowing it to be normalized or some vectors negated.
 /// Note that if trying to negate a vector that isn't used in the definition of the orthogonal frame, an error will be raised.
-#[derive(Copy, Clone, Debug, PartialEq, Serialize, Deserialize, StaticType)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct ReferenceFrame {
     pub frame: OrthogonalFrame,
     pub negate_x: bool,
     pub negate_y: bool,
     pub negate_z: bool,
 }
-*/
+
+impl Almanac {}
 
 #[cfg(test)]
-mod ut_analysis_dhall {
+mod ut_analysis {
 
-    use crate::analysis::{FrameUid, ScalarCalc, State, VectorExpr};
+    use crate::analysis::{FrameUid, OrbitalElement, ScalarCalc, StateDef, VectorExpr};
+    use crate::constants::celestial_objects::EARTH;
+    use crate::constants::frames::{EME2000, SUN_J2000};
+    use crate::constants::orientations::J2000;
     use crate::prelude::Almanac;
+    use arrow::array::Scalar;
     use rstest::*;
     use serde_dhall::StaticType;
 
+    use super::FrameDef;
+
     #[fixture]
-    pub fn almanac() -> Almanac {
+    fn almanac() -> Almanac {
         use std::path::PathBuf;
 
         let manifest_dir =
@@ -105,78 +116,23 @@ mod ut_analysis_dhall {
         .unwrap()
     }
 
-    #[test]
-    fn test_vector_expr_fixed() {
-        let v = VectorExpr::Fixed {
-            x: 1.0,
-            y: 2.0,
-            z: 3.0,
-        };
-        let v_str = serde_dhall::serialize(&v)
-            .static_type_annotation()
-            .to_string()
-            .unwrap();
-        println!("{v_str:?}");
-        let v_deser: VectorExpr = serde_dhall::from_str(&v_str).parse().unwrap();
-        assert_eq!(v_deser, v);
-    }
+    #[rstest]
+    fn test_analysis_orbital_element(almanac: Almanac) {
+        // Try to compute the SMA of the Earth with respect to the Sun.
 
-    #[test]
-    fn test_vector_expr_state() {
-        let state = State {
-            from_frame: FrameUid {
-                ephemeris_id: 399,
-                orientation_id: 0,
-            },
-            to_frame: FrameUid {
-                ephemeris_id: 301,
-                orientation_id: 0,
-            },
-        };
-        let pos = VectorExpr::Position(state);
+        let from_frame = FrameDef::Uid(EME2000.into());
+        let to_frame = FrameDef::Uid(SUN_J2000.into());
 
-        let pos_str = serde_dhall::serialize(&pos)
-            .static_type_annotation()
-            .to_string()
-            .unwrap();
-        println!("{pos_str:?}");
-        let v_deser: VectorExpr = serde_dhall::from_str(&pos_str).parse().unwrap();
-        assert_eq!(v_deser, pos);
-    }
-
-    #[test]
-    fn test_orbit_calculation() {
-        let state_m2e = State {
-            from_frame: FrameUid {
-                ephemeris_id: 301,
-                orientation_id: 0,
-            },
-            to_frame: FrameUid {
-                ephemeris_id: 399,
-                orientation_id: 0,
-            },
+        let state = StateDef {
+            from_frame,
+            to_frame,
         };
 
-        let pos_m2e = VectorExpr::Position(state_m2e);
-
-        let expr_type = VectorExpr::static_type().to_string();
-        println!("{expr_type}");
-
-        let scalars = vec![
-            ScalarCalc::OrbitalElement(crate::analysis::OrbitalElement::SemiMajorAxis(state_m2e)),
-            ScalarCalc::Norm(pos_m2e),
+        let calculations = vec![
+            ScalarCalc::OrbitalElement(OrbitalElement::SemiMajorAxis(state)),
+            ScalarCalc::OrbitalElement(OrbitalElement::Eccentricity(state)),
         ];
 
-        let scalars_str = serde_dhall::serialize(&scalars)
-            .static_type_annotation()
-            .to_string()
-            .unwrap();
-        println!("{scalars_str:?}");
-        /*let scalars_deser: Vec<ScalarCalc> = serde_dhall::from_str(&scalars_str).parse().unwrap();
-
-        assert_eq!(scalars_deser, scalars);*/
-
-        let sexpr_str = serde_lexpr::to_value(&scalars).unwrap();
-        println!("{sexpr_str}");
+        // almanac.calculate(calculations).unwrap();
     }
 }
