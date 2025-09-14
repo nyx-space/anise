cf37a3fadda10a2ef8814d942ed4453a834d4fd2
Christopher Rabotin <christopher.rabotin@gmail.com>
2025-09-14 00:22:29 -0600

    Add dhall serialization to Location.
    I'll add a feature to load a whole LocationDataSet from Dhall because
    that makes it much easier to version control than its binary equivalent.
    However, since ANISE will handle everything from ASN.1 binary, the Dhall
    will be converted to binary before being used.

--- anise/src/structure/location.rs
+++ anise/src/structure/location.rs
@@ -8,31 +8,43 @@
  * Documentation: https://nyxspace.com/
  */
 use core::fmt;
-use der::{asn1::OctetStringRef, Decode, Encode, Error, ErrorKind, Length, Reader, Writer};
+use der::{Decode, Encode, Reader, Writer};
 
-use crate::frames::FrameUid;
+use crate::NaifId;
+
+use serde_derive::{Deserialize, Serialize};
+
+#[cfg(feature = "analysis")]
+use serde_dhall::StaticType;
 
 #[cfg(feature = "python")]
 use pyo3::prelude::*;
+
 /// Location is defined by its latitude, longitude, height above the geoid, mean angular rotation of the geoid, and a frame UID.
 /// If the location includes a terrain mask, it will be used for obstruction checks when computing azimuth and elevation.
 /// **Note:** The mean Earth angular velocity is `0.004178079012116429` deg/s.
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 #[cfg_attr(feature = "python", pyclass)]
+#[cfg_attr(feature = "analysis", derive(StaticType))]
 #[cfg_attr(feature = "python", pyo3(module = "anise.astro"))]
 pub struct Location {
     pub latitude_deg: f64,
     pub longitude_deg: f64,
     pub height_km: f64,
-    pub frame: FrameUid,
-    pub mask: Vec<TerrainMask>,
+    /// ephemeris ID of the celestial object on which this location tests
+    pub ephemeris_id: NaifId,
+    /// orientation ID of the body-fixed frame on which this location rests
+    pub orientation_id: NaifId,
+    /// Mask due to the terrain; vector is assumed to be pre-sorted by azimuth (or the mask will not work correctly)
+    pub terrain_mask: Vec<TerrainMask>,
 }
 
 /// TerrainMask is used to compute obstructions during AER calculations.
-#[derive(Clone, Default, Debug, PartialEq)]
+#[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
+#[cfg_attr(feature = "analysis", derive(StaticType))]
 pub struct TerrainMask {
     /// Azimuth where this elevation mask starts.
-    pub start_azimuth_deg: f64,
+    pub azimuth_deg: f64,
     pub elevation_mask_deg: f64,
 }
 
@@ -40,53 +52,72 @@ impl TerrainMask {
     /// Initializes a new flat terrain mask.
     pub fn from_flat_terrain(elevation_mask_deg: f64) -> Self {
         Self {
-            start_azimuth_deg: 0.0,
+            azimuth_deg: 0.0,
             elevation_mask_deg,
         }
     }
 }
 
-/* impl Encode for Semver {
+impl Encode for TerrainMask {
     fn encoded_len(&self) -> der::Result<der::Length> {
-        let data: [u8; 3] = [self.major, self.minor, self.patch];
-        let as_octet_string = OctetStringRef::new(&data).unwrap();
-        as_octet_string.encoded_len()
+        self.azimuth_deg.encoded_len()? + self.elevation_mask_deg.encoded_len()?
     }
 
     fn encode(&self, encoder: &mut impl Writer) -> der::Result<()> {
-        let data: [u8; 3] = [self.major, self.minor, self.patch];
-        let as_octet_string = OctetStringRef::new(&data).unwrap();
-        as_octet_string.encode(encoder)
+        self.azimuth_deg.encode(encoder)?;
+        self.elevation_mask_deg.encode(encoder)
     }
 }
 
-impl<'a> Decode<'a> for Semver {
+impl<'a> Decode<'a> for TerrainMask {
     fn decode<R: Reader<'a>>(decoder: &mut R) -> der::Result<Self> {
-        let data: OctetStringRef = decoder.decode()?;
-        if data.len() != Length::new(3) {
-            return Err(Error::new(
-                ErrorKind::Incomplete {
-                    expected_len: Length::new(3),
-                    actual_len: data.len(),
-                },
-                Length::new(0),
-            ));
-        }
-
         Ok(Self {
-            major: data.as_bytes()[0],
-            minor: data.as_bytes()[1],
-            patch: data.as_bytes()[2],
+            azimuth_deg: decoder.decode()?,
+            elevation_mask_deg: decoder.decode()?,
         })
     }
 }
 
-impl fmt::Display for Semver {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(
-            f,
-            "ANISE version {}.{}.{}",
-            self.major, self.minor, self.patch
-        )
+#[cfg(test)]
+mod ut_loc {
+    use super::Location;
+    use crate::constants::celestial_objects::EARTH;
+    use crate::constants::orientations::ITRF93;
+    #[cfg(feature = "analysis")]
+    #[test]
+    fn test_dhall_location() {
+        use serde_dhall::serialize;
+
+        use crate::structure::location::TerrainMask;
+
+        let dss65 = Location {
+            latitude_deg: 40.427_222,
+            longitude_deg: 4.250_556,
+            height_km: 0.834_939,
+            ephemeris_id: EARTH,
+            orientation_id: ITRF93,
+            terrain_mask: vec![
+                TerrainMask {
+                    azimuth_deg: 0.0,
+                    elevation_mask_deg: 5.0,
+                },
+                TerrainMask {
+                    azimuth_deg: 35.0,
+                    elevation_mask_deg: 10.0,
+                },
+                TerrainMask {
+                    azimuth_deg: 270.0,
+                    elevation_mask_deg: 3.0,
+                },
+            ],
+        };
+
+        println!(
+            "{}",
+            serialize(&dss65)
+                .static_type_annotation()
+                .to_string()
+                .unwrap()
+        );
     }
-} */
+}
