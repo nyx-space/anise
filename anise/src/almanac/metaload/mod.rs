8f4141ae825060665d9446b5936e51314bf9445d
Christopher Rabotin <christopher.rabotin@gmail.com>
2025-10-21 22:21:50 -0500

    fix test_from_dhall for meta almanac

--- anise/src/almanac/metaload/mod.rs
+++ anise/src/almanac/metaload/mod.rs
@@ -144,10 +144,10 @@ mod meta_test {
   [ { crc32 = Some 0x7286750a
     , uri = "http://public-data.nyxspace.com/anise/de440s.bsp"
     }
-  , { crc32 = Some 0x3503391
+  , { crc32 = Some 0x51f69e46
     , uri = "http://public-data.nyxspace.com/anise/v0.7/pck11.pca"
     }
-  , { crc32 = Some 0xabd5ff11
+  , { crc32 = Some 0x32c8f9d7
     , uri = "http://public-data.nyxspace.com/anise/v0.7/moon_fk_de440.epa"
     }
   , { crc32 = Some 0xcde5ca7d
