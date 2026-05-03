65dce06f38d02ff9c4deca5fdd6e0e75fa69fdf5
Christopher Rabotin <christopher.rabotin@gmail.com>
2026-05-03 08:58:07 -0600

    Update moon fk CRC in tests

--- anise/src/almanac/metaload/mod.rs
+++ anise/src/almanac/metaload/mod.rs
@@ -183,7 +183,7 @@ mod meta_test {
   , { crc32 = Some 0x1edb3eac
     , uri = "http://public-data.nyxspace.com/anise/v0.10/pck11.pca"
     }
-  , { crc32 = Some 0x32c8f9d7
+  , { crc32 = Some 0xc6c252fa
     , uri = "http://public-data.nyxspace.com/anise/v0.10/moon_fk_de440.epa"
     }
   , { crc32 = Some 0xcde5ca7d
