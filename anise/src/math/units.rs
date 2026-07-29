cbc1863ac5a3950d4ae7c339af4ca63f107a4be6
Mouli <duddupudimouli@gmail.com>
2026-07-29 16:22:33 +0530

    fix length unit conversion methods

--- anise/src/math/units.rs
+++ anise/src/math/units.rs
@@ -24,10 +24,10 @@ pub enum LengthUnit {
 }
 
 impl LengthUnit {
-    /// Returns the conversion factor of this distance unit to meters.
-    /// E.g. To convert Self::Kilometers into Self::Meters, multiply by 1e-3.
+    /// Returns the conversion factor of this distance unit from meters.
+    /// E.g. To convert Self::Meters into Self::Kilometers, multiply by 1e-3.
     #[must_use]
-    pub const fn to_meters(&self) -> f64 {
+    pub const fn from_meters(&self) -> f64 {
         match self {
             Self::Micrometer => 1e6,
             Self::Millimeter => 1e3,
@@ -37,10 +37,10 @@ impl LengthUnit {
         }
     }
 
-    /// Returns the conversion factor of this distance unit from meters.
+    /// Returns the conversion factor of this distance unit to meters.
     /// E.g. To convert Self::Kilometers into Self::Meters, multiply by 1e3.
     #[must_use]
-    pub const fn from_meters(&self) -> f64 {
+    pub const fn to_meters(&self) -> f64 {
         match self {
             Self::Micrometer => 1e-6,
             Self::Millimeter => 1e-3,
